19b9f05099079c9266095bf6f2359deaaf332b74
Daniel Jurado <danieljp.85@gmail.com>
2023-11-30 23:48:29 +0100

    input control

--- src/pages/auth/loginPage.js
+++ src/pages/auth/loginPage.js
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import Button from "../../components/button";
 import { login } from "./service";
 
 function LoginPage({ onLogin }) {
+  
+  const [credentials, setCredentials] = useState({
+    email: '', 
+    password: '',
+  })
   const handleSubmit = async (event) => {
     event.preventDefault();
-    await login({
-      email: event.target.email.value,
-      password: event.target.password.value,
-    });
+    await login(credentials);
     onLogin();
   };
+
+  const handleEmailChange = (event) => {
+    setCredentials({email: event.target.value, password: credentials.password,});
+  };
+
+  const handlePasswordChange = (event) => {
+    setCredentials({email: credentials.email, password: event.target.value});
+  };
+
+  const disabled = !(credentials.email && credentials.password);
 
   return (
     <div>
       <h1>Log in to Wallapop</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" />
-        <input type="password" name="password" />
-        <Button type="submit" $variant="primary">
+        <input type="email" name="email" onChange={handleEmailChange} value={credentials.email} />
+        <input
+          type="password"
+          name="password"
+          onChange={handlePasswordChange}
+          value={credentials.password}
+        />
+        <Button type="submit" $variant="primary" disabled={disabled}>
           Log in
         </Button>
       </form>
